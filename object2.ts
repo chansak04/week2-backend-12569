1881d257893890b0500068c60be6221ea4b74ea3
chansak04 <deawchansak@gmail.com>
2026-05-26 17:09:58 +0700

    up

--- object2.ts
+++ object2.ts
@@ -15,3 +15,5 @@ function greet(user: User): string {
     return `Hello, ${user.name}! You are ${user.age} years old.`;
 }
 console.log(greet(user)); // Output: Hello, Alice! You are 30 years old.
+
+console.log(JSON.stringify(user, null, 2)); // Output: {"name":"Alice","age":30}
